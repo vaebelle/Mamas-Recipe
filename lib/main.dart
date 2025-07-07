69cd01ac930aecd934c693a01964662f56e29714
pioloebarle <besingapiolo@gmail.com>
2025-07-07 18:37:35 +0800

    Authentication Firebase (Google Sign In and Email)  and Database Firestore Setup

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:mama_recipe/screens/home.dart';
 import 'package:mama_recipe/screens/login.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseFirestore.instance.settings = const Settings(
+    persistenceEnabled: true,
+  );
   runApp(const MyApp());
 }
 
