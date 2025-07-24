398ba2b5b493cacf9a29d36ba12c42bf29f3ffcd
pioloebarle <besingapiolo@gmail.com>
2025-07-24 23:11:15 +0800

    Update: Google Button Icon

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -1,30 +1,33 @@
 import 'package:flutter/cupertino.dart';
 import 'package:mama_recipe/widgets/sharedPreference.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Button extends StatelessWidget {
   final Function()? onTap;
   final String text;
-  final borderRadius;
-  final color;
-  final textColor;
-  final BoxBorder? border;
+  final Color? color;
+  final Color? textColor;
+  final double? borderRadius;
+  final Border? border;
+  final String? iconPath; // Add this parameter for icon support
 
   const Button({
     super.key,
     required this.onTap,
     required this.text,
+    this.color,
+    this.textColor,
     this.borderRadius = 12.0,
-    this.color = CupertinoColors.systemBlue,
-    this.textColor = CupertinoColors.white,
     this.border,
+    this.iconPath, // Add this parameter
   });
 
   @override
   Widget build(BuildContext context) {
     final isDarkMode = SharedPreferencesHelper.instance.isDarkMode;
 
-    Color finalColor = color;
-    Color finalTextColor = textColor;
+    Color finalColor = color ?? CupertinoColors.black;
+    Color finalTextColor = textColor ?? CupertinoColors.white;
     BoxBorder? finalBorder = border;
 
     if (color == CupertinoColors.white && isDarkMode) {
@@ -36,22 +39,42 @@ class Button extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        padding: const EdgeInsets.all(12.0),
-        margin: const EdgeInsets.symmetric(horizontal: 25.0),
+        padding: const EdgeInsets.symmetric(
+          vertical: 11,
+          horizontal: 20,
+        ), // REDUCED PADDING
+        margin: const EdgeInsets.symmetric(horizontal: 25),
         decoration: BoxDecoration(
           color: finalColor,
-          borderRadius: BorderRadius.circular(borderRadius),
+          borderRadius: BorderRadius.circular(borderRadius ?? 8),
           border: finalBorder,
         ),
         child: Center(
-          child: Text(
-            text,
-            style: TextStyle(
-              color: finalTextColor,
-              fontSize: 16.0,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
+          child: iconPath != null
+              ? Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Image.asset(iconPath!, width: 20, height: 20),
+                    const SizedBox(width: 10),
+                    Text(
+                      text,
+                      style: TextStyle(
+                        color: finalTextColor,
+                        fontWeight:
+                            FontWeight.w500, // REVERTED TO ORIGINAL BOLD
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                )
+              : Text(
+                  text,
+                  style: TextStyle(
+                    color: finalTextColor,
+                    fontWeight: FontWeight.w500, // REVERTED TO ORIGINAL BOLD
+                    fontSize: 16,
+                  ),
+                ),
         ),
       ),
     );
